c2d82fbbf6088056631a8126d1e465f554bf11b9
Maxim Belyi <btf9458@gmail.com>
2025-11-27 17:58:27 +0500

    Добавлен класс для работы с пользователями

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+<?php
+global $APPLICATION;
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Мебельная компания");
-//?>
-</p><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+
+require($_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/userHelper.php");
+
+$newUserFields = [
+    "LOGIN" => "test_user1",
+    "NAME" => "иван",
+    "LAST_NAME" => "факов",
+    "PASSWORD" => "123456",
+    "CONFIRM_PASSWORD" => "123456",
+    "EMAIL" => "test@test.ru",
+    "ACTIVE" => "Y",
+    "UF_MY_STRING" => "какая то строка",
+    "UF_MY_NUMBER" => 1443239994,
+    "UF_MY_BOOL" => TRUE,
+];
+
+echo UserHelper::createNewUser($newUserFields);
+echo "<pre>";
+print_r(UserHelper::getList());
+echo "</pre>"
+?>
+  <?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
--- local/php_interface/userHelper.php
+++ local/php_interface/userHelper.php
@@ -1,6 +1,43 @@
 <?php
 
+use Bitrix\Main\UserTable;
 class userHelper
 {
+    public static function createNewUser($fields):string
+    {
+        $user = new CUser;
+        $ID = $user->Add($fields);
+        if (intval($ID) > 0) {
+            return "Пользователь создан с ID: " . $ID;
+        } else {
+            return "Ошибка: " . $user->LAST_ERROR;
+        }
+    }
+    public static function getList($filter = [], $order = ['ID' => 'ASC'])
+    {
+        $result = UserTable::getList([
+            'select' => ['ID', 'NAME', 'ACTIVE', 'UF_*'],
+            'filter' => $filter,
+            'order' => $order,
+        ]);
+        return $result->fetchAll();
+        return [];
+    }
+    public static function updateByFilter($filter, $newFields):void
+    {
+        $usersList = self::getList($filter);
+        $userObject = new CUser;
+        foreach ($usersList as $newUserData) {
+            $userId = $newUserData['ID'];
+            $result = $userObject->Update($userId, $newFields);
 
+            if (!$result) {
+                echo "Что то пошло не так для пользователя $userId: " . $userObject->LAST_ERROR->LAST_ERROR;
+            }
+        }
+    }
+    public static function deleteInactiveUser()
+    {
+
+    }
 }
